518290f03cfe03baedea56619f33a98493c2e3b6
sam babu <48213345+sambabu21@users.noreply.github.com>
2024-04-10 19:50:32 +0530

    Update Index.jsx

--- designo/src/Components/Company/Index.jsx
+++ designo/src/Components/Company/Index.jsx
@@ -2,8 +2,14 @@ import Banner from "./Banner";
 import Content from "./Content";
 import FooterContent from "./FooterContent";
 import Locations from "./Locations";
+import { useEffect } from "react"
+
 
 export default function AboutUs(){
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[])
+
     return(
         <div className=" mb-[400px]">
             <Banner />
