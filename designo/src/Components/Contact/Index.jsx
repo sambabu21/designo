d93eddadd009bc0eda9dce3157194a96b29f24ff
sam babu <48213345+sambabu21@users.noreply.github.com>
2024-04-10 19:51:27 +0530

    Update Index.jsx

--- designo/src/Components/Contact/Index.jsx
+++ designo/src/Components/Contact/Index.jsx
@@ -1,7 +1,13 @@
 import Locations from "../Company/Locations";
 import ContactForm from "./ContactForm";
+import { useEffect } from "react"
+
 
 export default function Contact(){
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[])
+
     return(
         <div className="mb-[400px]">
             <ContactForm />
