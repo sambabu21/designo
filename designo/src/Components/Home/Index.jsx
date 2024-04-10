6af621c2e7cd34df568f0c019151f668b9d0d9f6
sam babu <48213345+sambabu21@users.noreply.github.com>
2024-04-10 19:48:10 +0530

    Update Index.jsx

--- designo/src/Components/Home/Index.jsx
+++ designo/src/Components/Home/Index.jsx
@@ -1,8 +1,14 @@
 import FeatureSection from "./FeatureSection";
 import Hero from "./Hero";
 import Qualities from "./Qualities";
+import { useEffect } from "react"
+
 
 export default function Home(){
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[])
+
     return (
         <div className="flex flex-col justify-center mb-[400px]">
             <Hero />
