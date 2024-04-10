5398f9f40a649262f17b22817b1fe20e8db723a4
sam babu <48213345+sambabu21@users.noreply.github.com>
2024-04-10 19:51:05 +0530

    Update Index.jsx

--- designo/src/Components/Location/Index.jsx
+++ designo/src/Components/Location/Index.jsx
@@ -1,6 +1,12 @@
 import Content from "./Content";
+import { useEffect } from "react"
+
 
 export default function Locations(){
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[])
+
     return (
         <div className=" mb-[400px]">
             <Content />
