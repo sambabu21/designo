f4c7ef3a6488528a044d02dea86d2b006c71843b
sam babu <48213345+sambabu21@users.noreply.github.com>
2024-04-10 19:50:06 +0530

    Update Index.jsx

--- designo/src/Components/Graphic Design/Index.jsx
+++ designo/src/Components/Graphic Design/Index.jsx
@@ -1,8 +1,14 @@
 import Banner from "./Banner";
 import OtherLinks from "./OtherLinks";
 import ProjectList from "./ProjectList";
+import { useEffect } from "react"
+
 
 export default function GraphicDesign(){
+    useEffect(()=>{
+        window.scrollTo(0,0)
+    },[])
+
     return(
         <div className=" mb-[400px]">
             <Banner />
